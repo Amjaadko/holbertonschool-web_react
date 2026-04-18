3d736d8fd1a8395dc7d57a9ff0101317a1d5f060
Amjad Al Omani <iamjaadk@gmail.com>
2026-04-18 23:12:26 +0000

    Task 0 TypeScript

---
+++ TypeScript/task_0/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      templateContent: `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Task 0</title>
+          </head>
+          <body></body>
+        </html>
+      `,
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+};
